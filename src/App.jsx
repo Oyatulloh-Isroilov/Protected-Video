380ccb64ded51861ac65efdc201dad7225d3547f
Oyatulloh-Isroilov <oyatullohisroilov5@gmail.com>
2024-03-23 17:21:55 +0500

    protected video

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+
+function App() {
+
+  return (
+    <>
+      <div className="container">
+        <iframe src="https://player.vimeo.com/video/926567610?badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479" width="1200" height="675" frameborder="0" allow="autoplay; fullscreen; picture-in-picture; clipboard-write; gyroscope; accelerometer" title="AnimationBtn1"></iframe>
+      </div>
+    </>
+  )
+}
+
+export default App
